f46e8a6909e077acd9abe1c5b008e77b7473248b
zgh123zgh <972833647@qq.com>
2018-05-09 09:49:49 +0800

    111

--- Proxy/Proxy.cpp
+++ Proxy/Proxy.cpp
@@ -1,24 +1,21 @@
-// Proxy.cpp : 定义控制台应用程序的入口点。
+// SimpleFactory.cpp : 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
-
-
+#include "stdafx.h" 
+#include "IFactory.h" 
+#include "PhoneFactory.h" 
+#include "FoxconnProxy.h" 
+ 
 int main()
 {
+     
+    IFactory* factory = new PhoneFactory();
+    FoxconnProxy* proxy = new FoxconnProxy(factory);
+    proxy->makeProduct();
 
+    delete factory;
 
-
-
-
-
-
-
-
-
-
-
-
+    getchar();
     return 0;
 }
 
